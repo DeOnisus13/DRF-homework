78b1f7d565cb68a09694e04f2db66f88e76ee3ef
Denis Antropov <de94onisus@gmail.com>
2024-05-21 01:46:46 +0400

    HW_2 (24.2) add lessons_count for CourseSerializer

--- lms_app/serializers.py
+++ lms_app/serializers.py
@@ -1,10 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms_app.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
     """Сериализатор для курсов"""
+    lessons_count = SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        return obj.lesson.all().count()
 
     class Meta:
         model = Course
