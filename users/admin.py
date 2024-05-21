6b9dfa6163d8b57f6ac5dfba34b001d5a94053f1
Denis Antropov <de94onisus@gmail.com>
2024-05-22 00:26:33 +0400

    HW_2 (24.2) add model Payment

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payment
 
 admin.site.register(User)
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ("user", "course", "lesson", "payment_amount", "payment_method",)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
 from config.settings import NULLABLE
+from lms_app.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -22,3 +24,23 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    """Модель платежей"""
+
+    PAYMENT_METHOD_CHOICES = {"cash": "Наличные", "card": "Перевод на счет"}
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    payment_date = models.DateTimeField(default=timezone.now, **NULLABLE, verbose_name="Дата оплаты")
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Оплаченный курс")
+    lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, **NULLABLE, verbose_name="Оплаченный урок")
+    payment_amount = models.PositiveIntegerField(verbose_name="Сумма оплаты")
+    payment_method = models.CharField(max_length=15, choices=PAYMENT_METHOD_CHOICES, verbose_name="Способ оплаты")
+
+    def __str__(self):
+        return f"{self.user} - {self.payment_amount}р. - {self.course if self.course else self.lesson}"
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
