43fc6bcf4876c9d39f6e171c58e58499fad2f943
Denis Antropov <de94onisus@gmail.com>
2024-05-22 00:45:57 +0400

    HW_2 (24.2) добавлено поле вывода уроков в CourseSerializer

--- lms_app/serializers.py
+++ lms_app/serializers.py
@@ -3,21 +3,23 @@ from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lms_app.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    """Сериализатор для уроков"""
+
+    class Meta:
+        model = Lesson
+        fields = "__all__"
+
+
 class CourseSerializer(ModelSerializer):
     """Сериализатор для курсов"""
+
     lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(source="lesson", many=True)
 
     def get_lessons_count(self, obj):
         return obj.lesson.all().count()
 
     class Meta:
         model = Course
-        fields = "__all__"
-
-
-class LessonSerializer(ModelSerializer):
-    """Сериализатор для уроков"""
-
-    class Meta:
-        model = Lesson
         fields = "__all__"
