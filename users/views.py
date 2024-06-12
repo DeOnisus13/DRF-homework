241e982496ffcdead7c58317d621faaee7fab4c4
Denis Antropov <de94onisus@gmail.com>
2024-06-12 16:12:20 +0400

    Change UserViewSet and UserSerializer

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
@@ -18,8 +18,20 @@ class UserViewSet(ModelViewSet):
 
     def get_permissions(self):
         if self.action == "create":
-            self.permission_classes = (AllowAny,)
-        return super().get_permissions()
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save()
+
+    def perform_update(self, serializer):
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
 
 
 class PaymentCreateAPIView(CreateAPIView):
